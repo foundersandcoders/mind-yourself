485b24c4597ecc63db1ba9eb6fa0391eb04001fa
Mina <wilhelmina.jane@gmail.com>
2016-01-04 20:35:31 +0000

    unwatch bundle.js.  add watch script for webpack

--- src/app-container.jsx
+++ src/app-container.jsx
@@ -7,7 +7,7 @@ class AppContainer extends React.Component {
     render () {
         return (
             <div>
-                <h1>Success!</h1>
+                <h1>Successfully done!</h1>
             </div>
         );
     }
