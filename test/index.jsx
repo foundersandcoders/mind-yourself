201ad4d0bed6713ab07e8f5f1e57130231bbed78
Mina <wilhelmina.jane@gmail.com>
2016-01-08 17:38:56 +0000

    all views connected according to flowchart

--- test/index.jsx
+++ test/index.jsx
@@ -1,5 +1,9 @@
 'use strict';
-
+// unit tests
 import { appContainer } from './unit/app-container.js';
 import { landing } from './unit/landing.js';
 import { scenes } from './unit/scenes.js';
+
+
+// integration tests
+import { decisions } from './integration/decisions.js';
--- test/unit/landing.js
+++ test/unit/landing.js
@@ -2,14 +2,14 @@
 
 import React from 'react/dist/react-with-addons';
 import { test } from 'tape';
-import { shallowRender } from '../shallow-render.js';
+import { shallowRender } from '../utils/shallow-render.js';
 import { Landing } from '../../src/views/landing.jsx';
 
 
-test('Unit: Landing', (t) => {
+test('Unit: Landing renders as expected', (t) => {
 
     const rendered = shallowRender(Landing);
-    
+
     t.equal(rendered.type, 'div', 'Is of type "div"');
     t.equal(rendered.props.children.length, 2, 'Has two child components');
     t.end();
